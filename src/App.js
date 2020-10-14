78101ed31d32328484f5a08fa39f7f9218b0789c
Your Name <christianeze2012@gmail.com>
2020-10-13 18:47:27 -0700

    my initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component}from 'react';
+import MainNav from './Components/MainNav'
+
+class App extends Component{
+	render(){
+		return(
+			<div>
+				<p>
+					why
+				</p>
+				
+				<MainNav/>
+				
+			</div>
+		)
+	}
+	
+}
+
+
+ export default App 
---
+++ src/Components/MainNav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MainNav = () => {
+	return (
+		<div>
+			<div>
+			<p>LEARNABLE</p>
+			<p>AGORA</p>
+			<p>STARTZONE</p>
+			<p>ABOUT</p>
+			<p>CONTACT</p>
+			
+			</div>
+		</div>
+	);
+}
+
+export default MainNav;
